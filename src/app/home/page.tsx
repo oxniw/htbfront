24f901dff54f32e8184157baab6285a52dfef7c9
oxniw <oo6139116@gmail.com>
2025-06-02 23:50:27 +0700

    Added root route

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Towatch from "@/app/components/Towatch"
+import Link from 'next/link';
 function Page() {
   return (
     <div className="relative w-full h-screen">
@@ -13,6 +14,27 @@ function Page() {
         >
           <source src="/0525(1).mp4" type="video/mp4" />
         </video>
+        <header className="line-b fixed inset-x-0 top-0 z-20 flex h-14 items-center justify-between bg-transparent px-4 after:-bottom-px sm:px-6 text-white">
+          <div>
+            <h1 className='text-xl font-bold'> Create in TB18<span></span> </h1>
+          </div>
+          <div className='flex flex-1 justify-start pl-8'>
+          </div>
+          <p>
+              
+            My: {" "}
+            <Link
+              href="https://www.instagram.com/namnourng/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span>
+                instagram
+              </span>
+            </Link>
+            
+          </p>
+        </header>
         <div className="relative z-10 flex items-center justify-center h-full text-white flex-col gap-8">
           <h1 className="text-5xl font-bold lg:text-9xl md:text-8xl sm:text-6xl">View Point</h1>
           <Towatch/>
