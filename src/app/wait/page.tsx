9f085ec010da76b43d897a3a9ecb36f51b414289
oxniw <oo6139116@gmail.com>
2025-06-02 23:53:10 +0700

    Added root route

--- src/app/wait/page.tsx
+++ src/app/wait/page.tsx
@@ -20,6 +20,7 @@ function Page() {
      <div className="flex flex-col items-center justify-center h-screen space-y-4 bg-white ">
       <div className="w-16 h-16 border-4 border-purple-500 border-t-transparent rounded-full animate-spin"></div>
       <p className="text-purple-600 font-semibold text-lg">starting server...</p>
+      <p className="text-purple-600 font-semibold text-lg">around 60 s</p>
     </div>
   )
 }
